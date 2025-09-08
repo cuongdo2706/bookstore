89d43bac0b544fe6a5865d2d04585243729a2624
CRELLA\doman <domanhcuong27062003@gmail.com>
2025-09-08 15:31:56 +0700

    .

---
+++ frontend-online-store/src/app/app.ts
@@ -0,0 +1,14 @@
+import {Component, signal} from '@angular/core';
+import {JsonPipe} from "@angular/common";
+
+
+@Component({
+    selector: 'app-root',
+    imports: [
+    ],
+    templateUrl: './app.html',
+    styleUrl: './app.css'
+})
+export class App {
+
+}
