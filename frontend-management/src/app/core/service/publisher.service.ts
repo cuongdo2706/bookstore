bd55f29d6507135fb7868e652ac10a3aa522fa27
CRELLA <domanhcuong27062003@gmail.com>
2025-10-03 21:36:06 +0700

    .

---
+++ frontend-management/src/app/core/service/publisher.service.ts
@@ -0,0 +1,22 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {AppConstants} from "../../app.constants";
+import {ApiResponse} from "../model/response/api-response.model";
+import {PublisherResponse} from "../model/response/publisher-response.model";
+import {AttributeCreatedRequest} from "../model/request/attribute-created-request";
+import {AuthorResponse} from "../model/response/author-response.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PublisherService {
+    private http=inject(HttpClient);
+    private readonly url: string = AppConstants.API_BASE_URL + "publisher";
+    fetchPublishers() {
+        return this.http.get<ApiResponse<PublisherResponse[]>>(this.url);
+    }
+
+    savePublisher(value: AttributeCreatedRequest) {
+        return this.http.post<ApiResponse<PublisherResponse>>(this.url, value);
+    }
+}
