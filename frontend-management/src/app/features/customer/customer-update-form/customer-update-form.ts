f02a6100f714443a3c9aaecd0e820e178906194c
CRELLA <domanhcuong27062003@gmail.com>
2025-11-12 09:30:59 +0700

    .

---
+++ frontend-management/src/app/features/customer/customer-update-form/customer-update-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-form',
+  imports: [],
+  templateUrl: './customer-update-form.html',
+  styleUrl: './customer-update-form.css'
+})
+export class CustomerUpdateForm {
+
+}
