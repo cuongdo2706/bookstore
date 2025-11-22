b647b41917662711a17c114dd09d2cbf13e8afd5
CRELLA <domanhcuong27062003@gmail.com>
2025-11-22 18:09:05 +0700

    .

--- frontend-online-store/src/app/app.config.ts
+++ frontend-online-store/src/app/app.config.ts
@@ -1,35 +1,32 @@
-import {
-    ApplicationConfig,
-    importProvidersFrom,
-    provideBrowserGlobalErrorListeners,
-    provideZonelessChangeDetection
-} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection} from '@angular/core';
 import {provideRouter, withComponentInputBinding} from '@angular/router';
-import Aura from '@primeuix/themes/aura';
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import {routes} from './app.routes';
+import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 import {providePrimeNG} from "primeng/config";
+import Aura from "@primeuix/themes/aura";
 import {JwtModule} from "@auth0/angular-jwt";
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-      provideAnimationsAsync(),
-    provideRouter(routes, withComponentInputBinding()), provideClientHydration(withEventReplay()),
-      providePrimeNG({
-          ripple: true,
-          theme: {
-              preset: Aura,
-              options: {
-                  darkModeSelector: false,
-                  cssLayer: false
-              }
-          }
-      }),
-      // provideHttpClient(withInterceptors([authInterceptor])),
-      importProvidersFrom([JwtModule.forRoot({config: {tokenGetter: () => localStorage.getItem('token')}})]),
-  ]
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideZonelessChangeDetection(),
+        provideAnimationsAsync(),
+        providePrimeNG({
+            ripple: true,
+            theme: {
+                preset: Aura,
+                options: {
+                    darkModeSelector: false,
+                    cssLayer: false
+                }
+            }
+        }),
+        provideRouter(routes, withComponentInputBinding()),
+        provideClientHydration(withEventReplay()),
+        // provideHttpClient(withInterceptors([authInterceptor])),
+        importProvidersFrom([JwtModule.forRoot({config: {tokenGetter: () => localStorage.getItem('token')}})]),
+    ]
 };
